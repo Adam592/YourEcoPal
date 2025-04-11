fd57a6f007d1eca5a500cef29e97578ef406b038
EmilKulka <226310@student.uek.krakow.pl>
2025-04-12 00:06:17 +0200

    chore: nvm

--- src/pages/QRScannerPage/QRScannerPage.jsx
+++ src/pages/QRScannerPage/QRScannerPage.jsx
@@ -1,26 +1,8 @@
-import React, { useState } from 'react';
-import BarcodeScanner from './components/BarcodeScanner';
-import ProductDetails from './components/ProductDetails';
-import { Container, Row, Col } from 'react-bootstrap';
-
-function ProductScannerPage() {
-  const [scannedProduct, setScannedProduct] = useState(null);
-
-  const handleProductScanned = (product) => {
-    setScannedProduct(product);
-  };
-
-  return (
-    <Container>
-      <Row>
-        <Col>
-          <BarcodeScanner onProductScanned={handleProductScanned} />
-        </Col>
-        <Col>
-          {scannedProduct && <ProductDetails product={scannedProduct} />}
-        </Col>
-      </Row>
-    </Container>
-  );
+const QRScannerPage = () => {
+    return (
+        <>
+        <h1>THIS IS QR SCANNER PAGE</h1>
+        </>
+    )
 }
-export default ProductScannerPage;
+export default QRScannerPage;
